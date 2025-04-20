99fc82298853a8d6099c02303d31419baf11b7b6
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-04-20 19:04:03 -0300

    add project

---
+++ eduCombineApp/eduCombineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  eduCombineAppApp.swift
+//  eduCombineApp
+//
+//  Created by Dmitry Volkov on 20/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct eduCombineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
