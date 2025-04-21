ac56d0b257ffeb30a9ba6f95c30e843ae4b661b5
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-04-21 17:49:59 -0300

    Add assets and base bg

--- eduCombineApp/ContentView.swift
+++ eduCombineApp/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack(alignment: .top) {
+                Text("Hello, world!")
+                    .padding()
+            }
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(AccountBackground())
         }
-        .padding()
+        .toolbar(.hidden)
     }
 }
 
---
+++ eduCombineApp/TrackableScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  TrackableScrollView.swift
+//  eduCombineApp
+//
+//  Created by Dmitry Volkov on 20/04/2025.
+//
+
+import SwiftUI
+
+struct TrackableScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrackableScrollView()
+}
