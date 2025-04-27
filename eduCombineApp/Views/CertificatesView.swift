b45941a2a6edfac93ab9f6f5e32641c35050b8b8
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-04-26 22:51:24 -0300

    Add certificate card

---
+++ eduCombineApp/Views/CertificatesView.swift
@@ -0,0 +1,18 @@
+//
+//  CertificatesView.swift
+//  eduCombineApp
+//
+//  Created by Dmitry Volkov on 26/04/2025.
+//
+
+import SwiftUI
+
+struct CertificatesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CertificatesView()
+}
