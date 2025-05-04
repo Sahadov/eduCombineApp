160ed0c3f39df6273a0e62de377f3dba8bfbb16a
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-05-04 19:59:02 -0300

    Add TabView

--- eduCombineApp/Views/CertificatesView.swift
+++ eduCombineApp/Views/CertificatesView.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct CertificatesView: View {
+    @StateObject var certificateMV = CertificateViewModel()
+    @State private var selection: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selection) {
+            ForEach(certificateMV.certificates) { certificate in
+                Text("1")
+                //CertificateCard(certificateVM: $certificateMV, selection: $selection)
+            }
+        }
+        .background(AccountBackground())
+        .tabViewStyle(PageTabViewStyle())
     }
 }
 
